cd3c50f17f83f43d89b98983ec4fe28ccf43d782
Vipul-422 <96078027+Vipul-422@users.noreply.github.com>
2023-03-10 22:00:27 +0530

    Mux, Adder, Sign_ext, Memory components added!

--- Phase 1/include/components.h
+++ Phase 1/include/components.h
@@ -5,40 +5,103 @@
 #include <vector>
 
 using namespace std;
+/*
+1) instruction memory's black box
+2) register file's black box
+3) ALU black box
+4) Data memory Black box
+5) Muxes (done)
+6) Sign Ext
+7) Adder
+*/
 
 #ifndef COMPONENTS_H
 #define COMPONENTS_H
 
 class ALU {
 
-    int op1, op2, operation;
+    int op1, op2;
     int process();
 
     public:
-        void input(int _op1, int _op2, int _operation);
+        int operation;
+        void input(int _op1, int _op2);
         int output();
-
 
 };
 ALU alu;
 
 
-class regfile {
+class Regfile {
 
     map<string, int> regs;
     string rd, rs1, rs2;
 
     public:
-        regfile();
+        Regfile();
         void input(vector<int> _rs1, vector<int> _rs2, vector<int> _rd={0,0,0,0,0});
-        void write(int data, bool rfwrite);
+        void write(int data, bool rfwrite=0);
         int op1();
         int op2();
 
 };
-regfile regs;
+Regfile regs;
+
+//instruction memory start
+class Memory
+{
+    bool iswrite; //0 for memory read and 1 for memory write
+    int address;
+    static unsigned int mem[100000];
+    public:
+        void input(int _address, bool _iswrite=0, int data=0);
+        int read(int _address);
+        int output();
+};
+
+Memory mem;
+//instruction memory ends
 
 
+//mux starts
+class Mux
+{
+    vector<int> input_lines; //this vector will store values of all input values coming in the mux
+    public:
+        int select_line; //use to select which input_line will be selected
+        void input(vector<int> _input_lines); //update vector<int> input_lines with the help of select_line
+        int output(); //mux_name.output() will directly give us value of input
+};
+
+Mux mux_op2select;
+Mux mux_resultselect;
+Mux mux_branchTargetSel;
+Mux mux_isbranch;
+//mux ends
+
+//adder start
+class Adder
+{
+    int i1;
+    int i2;
+    public:
+        void input(int _i1, int _i2);
+        int output();
+};
+
+Adder adder_pc;
+Adder adder_branch;
+//adder ends
+
+//sign ext starts
+class Sign_ext
+{
+    int num;
+    public:
+        void input(vector<int> num);
+        int output();
+};
+//sign ext ends
 
 
 #endif
--- Phase 1/src/components.cpp
+++ Phase 1/src/components.cpp
@@ -4,10 +4,9 @@
 
 
 // ALU start
-void ALU::input(int _op1, int _op2, int _operation) {
+void ALU::input(int _op1, int _op2) {
     op1 = _op1;
     op2 = _op2;
-    operation = _operation;
 }
 
 int ALU::process() {
@@ -72,14 +71,14 @@ int ALU::output() {
 
 
 // regfile start
-regfile::regfile() {
+Regfile::Regfile() {
     for(int i=0; i<32; i++) {
         string temp = "x" + to_string(i);
         regs[temp] = 0;
     }
 }
 
-void regfile::input(vector<int> _rs1, vector<int> _rs2, vector<int> _rd={0,0,0,0,0}) {
+void Regfile::input(vector<int> _rs1, vector<int> _rs2, vector<int> _rd={0,0,0,0,0}) {
     
     int ird=0, irs1=0, irs2=0;
     for(int i=0; i<_rd.size(); i--) {
@@ -92,16 +91,73 @@ void regfile::input(vector<int> _rs1, vector<int> _rs2, vector<int> _rd={0,0,0,0
     rs2 = to_string(irs2);
 }
 
-void regfile::write(int data, bool rfwrite) {
+void Regfile::write(int data, bool rfwrite=0) {
     if(rfwrite && rd!="x0") {
         regs[rd] = data;
     }
 }
 
-int regfile::op1() {
+int Regfile::op1() {
     return regs[rs1];
 }
-int regfile::op2() {
+int Regfile::op2() {
     return regs[rs2];
 }
 // regfile end
+
+//following part is for mux, adder, sign ext, memory used in processor(contributed by Vipul)
+//mux start
+void Mux::input(vector<int> _input_lines) //function declaration of input for mux
+{
+    for(int i=0;i<_input_lines.size();i++){
+        input_lines.push_back(_input_lines[i]);
+    }
+}
+int Mux::output() //function declaration of output for mux
+{
+    return input_lines[select_line];
+}
+//mux end
+
+//memory start
+void Memory::input(int _address, bool _iswrite, int data=0){
+    mem[address]=data;
+    address=_address;
+    iswrite=_iswrite;
+}
+int Memory::read(int _address){
+    return mem[_address];
+}
+int Memory::output(){
+    return mem[address];
+}
+//memory end
+
+//adder start
+void Adder::input(int _i1, int _i2){
+    i1=_i1;
+    i2=_i2;
+}
+int Adder::output(){
+    return i1+i2;
+}
+//adder end
+
+//sign ext start
+void Sign_ext::input(vector<int> _num){
+    int k=_num.size()-1;
+    num=0;
+    for(int i=0;i<k;i++){
+        if(i==0){
+            num-=_num[i]*(1<<k);
+        }
+        else{
+            num+=_num[i]*(1<<k);
+        }
+        k--;
+    }
+}
+int Sign_ext::output(){
+    return num;
+}
+//sign ext ends
