5d2c2c01a1448df5c8274782aa64684ca3a6311e
VIKALP DHALWAL <110767544+Dvikalp@users.noreply.github.com>
2023-04-26 14:56:45 +0530

    Update riscv.h

--- Phase 2 (pipelined processor)/include/riscv.h
+++ Phase 2 (pipelined processor)/include/riscv.h
@@ -2,7 +2,7 @@
 #define RISCV_H
 
 
-void run_riscvsim();
+void run_riscvsim(char * forwardd,char* branch_pred);
 void reset_proc();
 void load_program_memory(char* filename);
 void write_data_memory();
