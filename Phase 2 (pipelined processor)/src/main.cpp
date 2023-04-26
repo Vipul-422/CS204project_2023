d46cbad23432e83234af0fa24b11ccc8edf18a3d
VIKALP DHALWAL <110767544+Dvikalp@users.noreply.github.com>
2023-04-26 14:58:42 +0530

    Update main.cpp

--- Phase 2 (pipelined processor)/src/main.cpp
+++ Phase 2 (pipelined processor)/src/main.cpp
@@ -3,24 +3,19 @@
 
 #include "../include/riscv.h"
 #include <iostream>
+#include <string>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char** argv) {
 
-	int isPipelined;
-	std::cout << "Enter 0 for single cycle\n";
-	std::cout << "Enter 1 for pipelined\n";
-	std::cin >> isPipelined;
-
-  
   	//reset the processor
 	reset_proc();
 	//load the program memory
 	load_program_memory(argv[1]);
 	//run the simulator
-	run_riscvsim();
+	run_riscvsim(argv[3],argv[4]);
 
 	return 1;
 }
